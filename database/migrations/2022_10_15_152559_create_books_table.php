bbc294951bda2d85d3a077f586087478efd70dd4
akramulfata10 <superlay582@gmail.com>
2022-10-17 20:50:35 +0700

    menambah tabel rent_logs

--- database/migrations/2022_10_15_152559_create_books_table.php
+++ database/migrations/2022_10_15_152559_create_books_table.php
@@ -13,12 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->string('phone')->unique();
-            $table->text('address');
+            $table->string('title')->unique();
             $table->string('status')->default('inactive');
             $table->timestamps();
         });
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('books');
     }
 };
