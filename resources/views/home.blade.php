04014ec94442afcff1f84e60d83eddba19683e6c
akramulfata10 <superlay582@gmail.com>
2022-12-06 14:50:25 +0700

    menambhkan 2 section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400&family=Sen:wght@700&display=swap"
         rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
@@ -158,11 +159,40 @@
 
     .card:hover {
         transform: scale(1.05);
-        box-shadow: 0 10px 20px rgba(0, 0, 0, .12), 0 4px 8px rgba(0, 0, 0, .06);
+        box-shadow: 0 10px 20px rgba(0, 0, 0, .12), 0 4px 8px hsla(0, 0%, 0%, 0.06);
     }
 
     .card-khusus:hover {
         background-color: #FBF6EA;
+    }
+
+    .rectangle {
+        position: absolute;
+        width: 843px;
+        height: 30px;
+        right: 298px;
+        top: 1883px;
+        background: #FFD050;
+        transform: matrix(1, 0, 0, -1, 0, 0);
+    }
+
+    .rectangle1 {
+
+        position: absolute;
+        width: 282px;
+        height: 30px;
+        right: 90px;
+        top: 1883px;
+        background: #592EA9;
+        transform: matrix(1, 0, 0, -1, 0, 0);
+    }
+
+    .back {
+        position: absolute;
+        width: 1152px;
+        height: 500px;
+        top: 1913px;
+        background: #F4F0F8;
     }
 </style>
 
@@ -333,6 +363,269 @@
         </div>
     </div>
     {{-- akhir section --}}
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md">
+                <div class="rectangle"></div>
+                <div class="rectangle1"></div>
+            </div>
+            <div class="back">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="px-5 m-5 p-5 mt-5">
+                            <h4 class="fs-4"
+                                style="font-family: 'Inter'; font-style: normal; font-weight: 600; font-size: 16px; line-height: 20px;">
+                                ABOUT US</h4>
+                            <h2 class="mt-3"
+                                style="font-family: 'Sen'; font-style: normal; font-weight: 700; font-size: 36px; line-height: 48px; letter-spacing: -2px;color: #232536;">
+                                We
+                                are a community of content writers who share their
+                                learnings</h2>
+
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora provident dolorem nulla
+                                in sunt. Error modi minus nulla ad quam.</p>
+
+                            <button type="submit" class="border-0 rounded-0 background-transparant"
+                                style="font-family: 'Sen'; font-style: normal; display: flex; align-items: center; color: #592EA9;">Readme
+                                ></button>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="px-5 m-5 p-5 mt-5">
+                            <h4 class="fs-4"
+                                style="font-family: 'Inter'; font-style: normal; font-weight: 600; font-size: 16px; line-height: 20px;">
+                                Our mision</h4>
+                            <h3 class="mt-3"
+                                style="font-family: 'Sen'; font-style: normal; font-weight: 700; font-size: 28px; line-height: 40px; letter-spacing: -1px; color: #232536;">
+                                Creating valuable content for creatives all around the
+                                world</h3>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore nobis mollitia ipsa
+                                nisi, laborum ullam sequi aut ipsum velit laboriosam?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- bagian manda --}}
+    <div class="container p-5 text-white" style="margin-top:750px">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center mb-3" style="color: #232536;">List Of Authors</h2>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md me-3" style="width: 296px; height: 324px; background: #F4F4F4;">
+                    <img src="{{ asset('gambar/fata.jpg') }}" class="mx-auto d-block mt-3 border rounded-circle"
+                        alt="">
+                    <h3 class="text-dark text-center mt-3">Akramul Fata</h3>
+                    <p class="fs-6 mt-1 text-center text-secondary">Content Writer @Company</p>
+                    <div class="mt-2 text-center">
+                        <i class="bi bi-whatsapp text-dark me-2"></i>
+                        <i class="bi bi-instagram text-dark me-2"></i>
+                        <i class="bi bi-twitter text-dark me-2"></i>
+                        <i class="bi bi-linkedin text-dark me-2"></i>
+                    </div>
+                </div>
+                <div class="col-md me-3" style="width: 296px; height: 324px; background: #FBF6EA;">
+                    <img src="{{ asset('gambar/aidil.png') }}" class="mx-auto d-block mt-3 border rounded-circle"
+                        alt="">
+                    <h3 class="text-dark text-center mt-3">Aidil Fitrah</h3>
+                    <p class="fs-6 mt-1 text-center text-secondary">Content Writer @Company</p>
+                    <div class="mt-2 text-center">
+                        <i class="bi bi-whatsapp text-dark me-2"></i>
+                        <i class="bi bi-instagram text-dark me-2"></i>
+                        <i class="bi bi-twitter text-dark me-2"></i>
+                        <i class="bi bi-linkedin text-dark me-2"></i>
+                    </div>
+                </div>
+                <div class="col-md me-3" style="width: 296px; height: 324px; background: #F4F4F4;">
+                    <img src="{{ asset('gambar/manda.png') }}" class="mx-auto d-block mt-3 border rounded-circle"
+                        alt="">
+                    <h3 class="text-dark text-center mt-3">Amanda Fauza</h3>
+                    <p class="fs-6 mt-1 text-center text-secondary">Content Writer @Company</p>
+                    <div class="mt-2 text-center">
+                        <i class="bi bi-whatsapp text-dark me-2"></i>
+                        <i class="bi bi-instagram text-dark me-2"></i>
+                        <i class="bi bi-twitter text-dark me-2"></i>
+                        <i class="bi bi-linkedin text-dark me-2"></i>
+                    </div>
+                </div>
+                <div class="col-md" style="width: 296px;height: 324px; background: #F4F4F4;">
+                    <img src="{{ asset('gambar/fika.png') }}" class=" mx-auto d-block mt-3 border rounded-circle"
+                        alt="">
+                    <h3 class="text-dark text-center mt-3">Fika Adillah</h3>
+                    <p class="fs-6 mt-1 text-center text-secondary">Content Writer @Company</p>
+                    <div class="mt-2 text-center">
+                        <i class="bi bi-whatsapp text-dark me-2"></i>
+                        <i class="bi bi-instagram text-dark me-2"></i>
+                        <i class="bi bi-twitter text-dark me-2"></i>
+                        <i class="bi bi-linkedin text-dark me-2"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-md">
+                    <img src="{{ asset('gambar/Logo1.png') }}" class="mx-auto d-block" alt="">
+                </div>
+                <div class="col-md ">
+                    <img src="{{ asset('gambar/Logo2.png') }}" class="mx-auto d-block" alt="">
+                </div>
+                <div class="col-md ">
+                    <img src="{{ asset('gambar/Logo3.png') }}" class="mx-auto d-block" alt="">
+                </div>
+                <div class="col-md ">
+                    <img src="{{ asset('gambar/Logo4.png') }}" class="mx-auto d-block" alt="">
+                </div>
+                <div class="col-md ">
+                    <img src="{{ asset('gambar/Logo5.png') }}" class="mx-auto d-block" alt="">
+                </div>
+            </div>
+
+            <div class="row mt-5" style="background: #FBF6EA; height: 464px;">
+                <div class="col-md-6">
+                    <div class="px-5 m-5 p-5 mt-5">
+                        <h5 class="fs-3 text-dark mb-1"
+                            style="font-family: 'Inter'; font-style: normal; font-weight: 600; font-size: 16px; line-height: 20px;">
+                            TESTIMONIALs</h5>
+                        <h2 class="mt-2"
+                            style="font-family: 'Sen'; font-style: normal; font-weight: 700;font-size: 36px; line-height: 48px; letter-spacing: -2px; color: #232536;">
+                            What people say about our blog</h2>
+                        <p class="text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="px-5 m-1 p-5 mt-5">
+                        <h2
+                            style="font-family: 'Sen'; font-style: normal; font-weight: 700; font-size: 24px; line-height: 32px; color: #232536;">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</h2>
+
+                        <div class="mt-5">
+                            <a href="" class="text-decoration-none text-secondary font-weight ">
+                                <img src="{{ asset('gambar/Profilepicture.png') }}" class="" alt="">
+                                Akramul Fata
+                                Web Developer
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- end bagian manda --}}
+
+
+    {{-- bagian aidil --}}
+    <div class="container p-5 text-white">
+        <div class="row">
+            <div class="col-md-12">
+                <h2
+                    style="font-family: 'Sen'; font-style: normal; font-weight: 700; font-size: 36px; line-height: 48px;text-align: center; letter-spacing: -2px; color: #232536;">
+                    Join our team to be a part <br> of our story</h2>
+            </div>
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-md-12 ">
+                <p class="text-center"
+                    style="font-family: 'Inter'; font-style: normal; font-weight: 400; font-size: 16px; line-height: 28px; text-align: center; color: #6D6E76;">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    <br> br do eiusmod
+                    tempor incididunt.
+                </p>
+            </div>
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-md-12 text-center">
+                <button type="submit" class="btn btn-warning rounded-0"
+                    style="color: #232536;font-family: 'Sen'; font-style: normal; font-weight: 700;font-size: 18px; line-height: 24px;">join
+                    now</button>
+            </div>
+        </div>
+    </div>
+
+    <footer class="text-center text-white" style="background: #232536; height: 570px;">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand text-white" href="#">Navbar</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact As</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+        {{-- <div class="container mt-5 mb-5"
+            style="width: 1280px; height: 256px; left: 80px; top: 144px; background: #FFFFFF;opacity: 0.1;">
+            <div class="col-md-6">
+                <h2 class="text-danger">
+                    Subscribe to our news letter to get latest updates and news</h2>
+            </div>
+        </div> --}}
+        <div class="container mt-5 mb-5">
+            <div class="text-center" style="background-color: rgba(0, 0, 0, 0.2); height: 350px;">
+                <div class="row justify-content-center">
+                    <div class="col-md-5 mt-5 mb-5">
+                        <h2>Subscribe to our news letter to get latest
+                            updates and news</h2>
+                    </div>
+                    <div class="col-md-5 mt-5 mb-5">
+                        <label for="exampleDataList" class="form-label">Search</label>
+                        <input class="form-control" list="datalistOptions" id="exampleDataList"
+                            placeholder="Type to search...">
+                        <datalist id="datalistOptions">
+                            <option value="San Francisco">
+                            <option value="New York">
+                            <option value="Seattle">
+                            <option value="Los Angeles">
+                            <option value="Chicago">
+                        </datalist>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col align-self-start pe-5">
+                    Finstreet 118 2561 Fintown
+                    <br>
+                    Hello@finsweet.com 020 7993 2905
+                </div>
+                <div class="col align-self-end">
+                    <i class="bi bi-whatsapp text-white me-2"></i>
+                    <i class="bi bi-instagram text-white me-2"></i>
+                    <i class="bi bi-twitter text-white me-2"></i>
+                    <i class="bi bi-linkedin text-white me-2"></i>
+                </div>
+            </div>
+        </div>
+    </footer>
+    {{-- end bagian aidil --}}
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
     </script>
